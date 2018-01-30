d99a6da3123f10afe30989a0b59ce8e9c8e9efdb
IsraelLaguan <israel@vertexstudio.co>
2018-01-30 16:40:07 -0600

    swarmed

--- src/app.js
+++ src/app.js
@@ -1,39 +1,63 @@
-const IPFS = require('ipfs')
-const Room = require('ipfs-pubsub-room')
+'use strict'
+const React = require('react')
+const ipfsAPI = require('ipfs-api')
 
-const ipfs = new IPFS({
-  repo: repo(),
-  EXPERIMENTAL: {
-    pubsub: true
-  },
-  config: {
-    Addresses: {
-      Swarm: [
-        '/dns4/ws-star.discovery.libp2p.io/tcp/443/wss/p2p-websocket-star'
-      ]
+const ipfs = ipfsAPI('localhost', '5001')
+const stringToUse = 'hello world from webpacked IPFS'
+
+class App extends React.Component {
+  constructor (props) {
+    super(props)
+    this.state = {
+      id: null,
+      version: null,
+      protocol_version: null,
+      added_file_hash: null,
+      added_file_contents: null
     }
   }
-})
-
-ipfs.once('ready', () => ipfs.id((err, info) => {
-  if (err) { throw err }
-  console.log('IPFS node ready with address ' + info.id)
-
-  const room = Room(ipfs, 'ipfs-pubsub-demo')
-
-  room.on('peer joined', (peer) => console.log('peer ' + peer + ' joined'))
-  room.on('peer left', (peer) => console.log('peer ' + peer + ' left'))
-
-  // send and receive messages
-
-  room.on('peer joined', (peer) => room.sendTo(peer, 'Hello ' + peer + '!'))
-  room.on('message', (message) => console.log('got message from ' + message.from + ': ' + message.data.toString()))
-
-  // broadcast message every 2 seconds
-
-  setInterval(() => room.broadcast('hey everyone!'), 2000)
-}))
-
-function repo () {
-  return 'ipfs/pubsub-demo/' + Math.random()
+  componentDidMount () {
+    ipfs.id((err, res) => {
+      if (err) throw err
+      this.setState({
+        id: res.id,
+        version: res.agentVersion,
+        protocol_version: res.protocolVersion
+      })
+    })
+    ipfs.add([Buffer.from(stringToUse)], (err, res) => {
+      if (err) throw err
+      const hash = res[0].hash
+      this.setState({added_file_hash: hash})
+      ipfs.cat(hash, (err, res) => {
+        if (err) throw err
+        let data = ''
+        res.on('data', (d) => {
+          data = data + d
+        })
+        res.on('end', () => {
+          this.setState({added_file_contents: data})
+        })
+      })
+    })
+  }
+  render () {
+    return <div style={{textAlign: 'center'}}>
+      <h1>Everything is working!</h1>
+      <p>Your ID is <strong>{this.state.id}</strong></p>
+      <p>Your IPFS version is <strong>{this.state.version}</strong></p>
+      <p>Your IPFS protocol version is <strong>{this.state.protocol_version}</strong></p>
+      <div>
+        <div>
+          Added a file! <br />
+          {this.state.added_file_hash}
+        </div>
+        <div>
+          Contents of this file: <br />
+          {this.state.added_file_contents}
+        </div>
+      </div>
+    </div>
+  }
 }
+module.exports = App
